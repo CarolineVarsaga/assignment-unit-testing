9d8999901c5dd6c025f7473b7cb3851c6dc04d20
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-05-20 10:48:03 +0200

    :truck: moves createHtml and displayNoResult to a new file

--- src/ts/movieApp.ts
+++ src/ts/movieApp.ts
@@ -1,5 +1,7 @@
 import { IMovie } from "./models/Movie";
 import { getData } from "./services/movieService";
+import { createHtml } from "./htmlFunctions";
+import { displayNoResult } from "./htmlFunctions";
 
 let movies: IMovie[] = [];
 
@@ -32,29 +34,3 @@ export async function handleSubmit() {
     displayNoResult(container);
   }
 }
-
-export const createHtml = (movies: IMovie[], container: HTMLDivElement) => {
-  for (let i = 0; i < movies.length; i++) {
-    let movie = document.createElement("div");
-    let title = document.createElement("h3");
-    let img = document.createElement("img");
-
-    movie.classList.add("movie");
-    title.innerHTML = movies[i].Title;
-    img.src = movies[i].Poster;
-    img.alt = movies[i].Title;
-
-    movie.appendChild(title);
-    movie.appendChild(img);
-
-    container.appendChild(movie);
-  }
-};
-
-export const displayNoResult = (container: HTMLDivElement) => {
-  let noMessage = document.createElement("p");
-
-  noMessage.innerHTML = "Inga sökresultat att visa";
-
-  container.appendChild(noMessage);
-};
