37615fc38142043300cb3cdbf6d7ae6c037b9d15
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-05-22 09:35:22 +0200

    ✅ adds passing test

--- src/__tests__/movieApp.test.ts
+++ src/__tests__/movieApp.test.ts
@@ -2,25 +2,32 @@ import { init } from "../ts/movieApp";
 import * as functions from "./../ts/functions";
 
 describe("movieApp tests", () => {
-   let mockedHandleSubmit: jest.SpyInstance<Promise<void>, []>;
+    let mockedHandleSubmit: jest.SpyInstance<Promise<void>, []>;
 
-    beforeEach(() => {
-        mockedHandleSubmit = jest.spyOn(functions, "handleSubmit");        
+    beforeEach( () => {
         document.body.innerHTML = `
             <form id="searchForm">
-                <input type="text" id="searchText" value="Avengers" placeholder="Skriv titel här" />
+                <input type="text" id="searchText" placeholder="Skriv titel här" />
                 <button type="submit" id="search">Sök</button>
             </form>
-        `        
-        init(); 
-    })
+        `; 
+
+        mockedHandleSubmit = jest.spyOn(functions, "handleSubmit").mockReturnValue(new Promise<void>((resolve) => {
+            resolve();
+        }));
+    });
 
     afterEach(() => {
         mockedHandleSubmit.mockRestore();
-    });
+    }); 
+
+    test("it should call handleSubmit when form is submitted", () =>{
+        const form = document.querySelector("#searchForm") as HTMLFormElement;
+
+        init();       
+        
+        form.dispatchEvent(new Event("submit"));
     
-    test("it should call handleSubmit when form is submitted", () => {     
-       
-        expect(mockedHandleSubmit).toHaveBeenCalled();
-    })
-})
+        expect(mockedHandleSubmit).toHaveBeenCalledTimes(1);
+    });
+});
